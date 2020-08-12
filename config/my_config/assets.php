acf3e10183bed610286b3acc54c4e59ab54892d8
diksub <diki@dev.netkromsolution.com>
2020-08-12 23:31:00 +0700

    Menambahkan plugin select2

---
+++ config/my_config/assets.php
@@ -0,0 +1,9 @@
+<?php
+return[
+    'css' => array(
+        'select2' => '/vendor/select2/css/select2.min.css'
+    ),
+    'js' => array(
+        'select2' => '/vendor/select2/js/select2.full.min.js'
+    )
+];
