f3d54618e1ca7ec91653803915448320e53417e6
diksub <diki@dev.netkromsolution.com>
2020-09-22 22:02:33 +0700

    Update Layout Admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,27 +1,29 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    {{-- meta --}}
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1, shrink-to-fit=no">
     <link rel="icon" href="/img/loader.svg?>/loader.svg" type="image/x-icon">
+    @stack('meta')
 
     {{-- Csrf Token --}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+   {{-- title --}}
+   <title>@yield('title'){{ " | " . config('app.name', 'Laravel') }}</title>
 
     {{-- Scripts --}}
     <script src="{{ asset('js/loader.js') }}"></script>
 
     {{-- Fonts --}}
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     {{-- Styles --}}
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     {{-- Custom Styles From Child --}}
-    @yield('styles')
+    @stack('styles')
 </head>
 <body>
     <div id="app">
@@ -57,7 +59,7 @@
     <script src="{{ asset('js/app.js') }}"></script>
 
     {{-- custom script from child--}}
-    @yield('script')
+    @stack('script')
 
 </body>
 </html>
